b099f93a15c2bea71cdbd8ad2808cc070752ca86
Shayan <Shayansadeghi1996@gmail.com>
2020-11-28 16:36:18 +0330

    added SelectBaseEstimator Function

--- RUtil.R
+++ RUtil.R
@@ -32,3 +32,40 @@ GetMode = function(x){
   u[which((tabulate(match(x,u))) == max(tabulate((match(x,u)))))]
 }
 
+
+
+## select a random value and use three different ways to estimate that
+SelectBestEstimator = function(data){
+  output = c()
+  #select the first
+  rec = min(which(data == sample(data,1)))
+  newData  = data[-rec]
+  print(paste("Real Record is: ", data[rec]))  
+  #mean:
+  m1 = mean(newData)
+  d1 = abs(m1 - data[rec]) 
+  #median:
+  m2 = median(newData) 
+  d2 = abs(m2 - data[rec])
+  #mode:
+  m3 = GetMode(newData) 
+  d3 = min(unique(abs(m3 - data[rec])))
+  
+  #choose Minimum distance
+  minDis = min(d1, d2, d3)
+  
+  
+  if(minDis == d1){
+    output = append(output,paste("Mean: ", m1))
+  }
+  if(minDis == d2){
+    output = append(output, paste("Median: ", m2))
+  }
+  if(minDis == d3){
+    output = append(output,paste("Mode: ", m3))
+  }
+  print(paste(output,collapse = " & "))
+}
+
+
+
